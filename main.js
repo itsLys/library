2ebba83f477229cd1384d121aa1e85a62600a7d2
Lys <elyesseen@gmail.com>
2024-03-01 10:47:36 +0100

    Fix a bug with displayBooks displays on top of the previous display

--- main.js
+++ main.js
@@ -1,29 +1,62 @@
 const myLibrary = [];
 
-function Book(name, pages) {
+function Book(name, author, pages) {
   this.name = name;
+  this.author = author;
   this.pages = pages;
 }
 
-function addBook(name, pages) {
-  let book = new Book(name, pages);
+function addBook(name, author, pages) {
+  let book = new Book(name, author, pages);
   myLibrary.push(book);
 }
-
-addBook("Book n1", 3);
-
-addBook("Book n2", 9);
-
-addBook("Book n3", 8);
 
 const shelf = document.querySelector(".shelf");
 const bookCard = document.createElement("div");
 bookCard.classList.add("book");
 
-myLibrary.forEach((book) => {
-  let bookClone = bookCard.cloneNode(true);
-  bookClone.textContent = `name: ${book.name}, pages: ${book.pages}`;
-  console.log(book);
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    let bookClone = bookCard.cloneNode(true);
+    bookClone.textContent = `name: ${book.name}, author ${book.author}, pages: ${book.pages}`;
+    console.log(book);
+    shelf.appendChild(bookClone);
+  });
+}
 
-  shelf.appendChild(bookClone);
+const newBook = document.querySelector(".new-book-btn");
+const addBookModal = document.querySelector(".form-con");
+const closeModal = document.querySelector(".close-btn");
+
+newBook.addEventListener("click", () => {
+  addBookModal.classList.add("open");
 });
+
+document.addEventListener("click", (e) => {
+  if (e.target == closeModal || e.target == addBookModal)
+    addBookModal.classList.remove("open");
+});
+
+const newBookName = addBookModal.querySelector("#book-name");
+const newBookAuthor = addBookModal.querySelector("#author-name");
+const newBookPages = addBookModal.querySelector("#page-num");
+const bookForm = addBookModal.querySelector(".book-form");
+const submitBook = addBookModal.querySelector(".submit");
+
+submitBook.addEventListener("click", (e) => {
+  e.preventDefault();
+  let bookName = newBookName.value;
+  let bookAuthor = newBookAuthor.value;
+  let bookPages = newBookPages.value;
+  addBook(bookName, bookAuthor, bookPages);
+  removeAll();
+  displayBooks();
+  bookForm.reset();
+  addBookModal.classList.remove("open");
+  console.log(myLibrary);
+});
+function removeAll() {
+  while (shelf.firstChild) {
+    shelf.removeChild(shelf.firstChild);
+  }
+}
